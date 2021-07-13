e3f4796f5aa461caa078fc5bbcebd524ca108ad2
Vritta531 <57068777+Vritta531@users.noreply.github.com>
2021-07-13 22:26:32 +0200

    Click power increase button

--- game.js
+++ game.js
@@ -4,10 +4,15 @@ const moneybutton = document.querySelector("#moneybutton");
 const autoHeading = document.querySelector("#autoclicker");
 const autoclickerbutton = document.querySelector("#autobutton");
 const infoText = document.querySelector("#info");
+const clickpowerbutton = document.querySelector("#clickpowerbutton")
+const clickpowervalue = document.querySelector("#clickpower")
 
 //Game Variables
 let money = 0;
 let auto = 0;
+var autocost = 10;
+var clickpower = 1;
+var clickpowercost = 30;
 
 //Function
 const updateUI = () => {
@@ -18,9 +23,9 @@ const mouseout = () => {
     infoText.innerHTML = `Hover over something for more info`
 }
 
-//Event Listeners
+//Make money button
 moneybutton.addEventListener("click", () => {
-    money += 1;
+    money += clickpower;
     updateUI();
 });
 moneybutton.addEventListener("mouseover", () => {
@@ -32,9 +37,11 @@ moneybutton.addEventListener("mouseout", () => {
 
 //Autoclicker purchase button
 autoclickerbutton.addEventListener("click", () => {
-    if(money >= 10) {
-        money -= 10;
+    if(money >= autocost) {
+        money -= autocost;
         auto += 1;
+        autocost = autocost*2
+        autoclickerbutton.innerHTML = `Autoclicker || ${autocost} money`
     }
 });
 autoclickerbutton.addEventListener("mouseover", () => {
@@ -44,10 +51,24 @@ autoclickerbutton.addEventListener("mouseout", () => {
     mouseout();
 });
 
-//Hover over info
+//Click power button
+clickpowerbutton.addEventListener("click", () => {
+    if(money >= autocost) {
+        money -= clickpowercost;
+        clickpower += 1;
+        clickpowercost = clickpowercost*2
+        clickpowerbutton.innerHTML = `Increase click power by 1 || ${clickpowercost} money`
+        clickpowervalue.innerHTML = `Click power: ${clickpower}`
+    }
+});
+autoclickerbutton.addEventListener("mouseover", () => {
+    infoText.innerHTML = `Autoclicker makes money for you | 1 autoclicker makes 1 money every 1000ms`
+});
+autoclickerbutton.addEventListener("mouseout", () => {
+    mouseout();
+});
 
-
-//Game Loop
+//Game Loops
 window.setInterval(() => {
     money += auto;
 }, 1000);
