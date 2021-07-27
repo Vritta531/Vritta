b5fa77273e7e3010efad47338bc8d2ea73f7e63e
Vritta531 <57068777+Vritta531@users.noreply.github.com>
2021-07-27 11:56:41 +0200

    Stone things now work

--- game.js
+++ game.js
@@ -2,33 +2,49 @@
 const moneyHeading = document.querySelector("#money");
 const moneybutton = document.querySelector("#moneybutton");
 const autoHeading = document.querySelector("#autoclicker");
+const autostonecollectorstat = document.querySelector("#autostonecollectorstat")
+const pickaxepowerstat = document.querySelector("#pickaxepowerstat")
 const autoclickerbutton = document.querySelector("#autobutton");
 const infoText = document.querySelector("#info");
 const clickpowerbutton = document.querySelector("#clickpowerbutton")
 const clickpowervalue = document.querySelector("#clickpower")
 const uppickaxe = document.querySelector("#uppickaxe")
+const stoneHeading = document.querySelector("#stone")
+const upclickpowermultiplier = document.querySelector("#upclickpowermultiplier")
+const addclickpwr = document.querySelector("#addclickpwrstat")
+const pickaxebutton = document.querySelector("#pickaxebutton")
+const autostonecollector = document.querySelector("#autostone")
+const pickaxepowerbutton = document.querySelector("#pickaxepowerbutton")
 
 //Game Variables
-let money = 0;
-let auto = 0;
+var money = 0;
+var auto = 0;
 var autocost = 10;
-var clickpower = 1;
+var additionalclickpower = 0;
+var clickpowermultiplier = 1;
+var clickpower = (1+additionalclickpower)*clickpowermultiplier;
 var clickpowercost = 30;
 var stone = 0;
-var uppickaxebought = 0;
+var autostone = 0;
+var autostonecost = 50;
+var pickaxepowercost = 100;
+var additionalpickaxepower = 0;
+var pickaxepower = 1+additionalpickaxepower;
 
 //Functions
 const updateUI = () => {
     moneyHeading.innerHTML = `Wood: ${money}`
     autoHeading.innerHTML = `Autochoppers: ${auto}`
+    stoneHeading.innerHTML = `Stone: ${stone}`
 };
 const mouseout = () => {
     infoText.innerHTML = `Hover over something for more info`
-}
+};
 
 //Game Loops
 window.setInterval(() => {
     money += auto;
+    stone += autostone;
 }, 1000);
 
 window.setInterval(() => {
@@ -105,21 +121,73 @@ clickpowerbutton.addEventListener("mouseout", () => {
     mouseout();
 });
 
-//Stone things
+//Stone things visible or nah
 var appBanners = document.getElementsByClassName('stoneclass'); //Hiding all stoneclass things
 for (var i = 0; i < appBanners.length; i ++) {
     appBanners[i].style.display = 'none';
 }
 
+//Gather stone button
+pickaxebutton.addEventListener("click", () => {
+    stone += pickaxepower;
+    updateUI();
+});
+pickaxebutton.addEventListener("mouseover", () => {
+    infoText.innerHTML = `Click this button to gather stone`
+});
+pickaxebutton.addEventListener("mouseout", () => {
+    mouseout();
+});
+
+//Automatic stone collector
+autostonecollector.addEventListener("click", () => {
+    if(stone >= autostonecost) {
+        stone -= autostonecost;
+        autostone += 1;
+        autostonecost = autostonecost*2
+        autostonecollector.innerHTML = `Automatic stone collector || ${autocost} wood`
+        autostonecollectorstat.innerHTML = `Automatic stone collectors: ${autostone}`
+    }
+});
+autostonecollector.addEventListener("mouseover", () => {
+    infoText.innerHTML = `Automatic stone collector | 1 ASC gathers 1 stone every 1000ms`
+});
+autostonecollector.addEventListener("mouseout", () => {
+    mouseout();
+});
+
+//Pickaxe power increase
+pickaxepowerbutton.addEventListener("click", () => {
+    if(stone >= pickaxepowercost) {
+        stone -= pickaxepowercost;
+        pickaxepower += 1;
+        pickaxepowercost = pickaxepowercost*2
+        pickaxepowerbutton.innerHTML = `Increase pickaxeaxe power by 1 || ${pickaxepowercost} stone`
+        pickaxepowerstat.innerHTML = `Pickaxe power: ${pickaxepower}`
+    }
+});
+pickaxepowerbutton.addEventListener("mouseover", () => {
+    infoText.innerHTML = `Increases pickaxe power by 1`
+});
+pickaxepowerbutton.addEventListener("mouseout", () => {
+    mouseout();
+});
+
 //Upgrades
 uppickaxe.addEventListener("click", () => {
     if(money>=1000) {
         money -= 1000;
-        uppickaxebought = 1;
         uppickaxe.style.display = 'none';
         var appBanners = document.getElementsByClassName('stoneclass'); //Hiding all stoneclass things
 for (var i = 0; i < appBanners.length; i ++) {
     appBanners[i].style.display = 'block';
 }
     }
-})
+});
+
+upclickpowermultiplier.addEventListener("click", () => {
+    if(money>=666) {
+        money -= 666;
+        clickpowermultiplier += 1;
+        upclickpowermultiplier.style.display = 'none';
+    }});
